3d1d5203d10e7494c88d87f365421b986c78ce8a
sindurasaraswathi <sindurasaraswathi@gmail.com>
2024-02-18 23:33:09 -0500

    Adding plots

--- plot_results.py
+++ plot_results.py
@@ -10,6 +10,7 @@ import pandas as pd
 import ast
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 df = pd.read_csv('New_results/LN_results_random.csv')
 df = df.dropna()
@@ -21,64 +22,49 @@ def extract_field(num, col):
         extract_list.append(elem[num])
     return extract_list
 
-lnd_fee = extract_field(1, 'LND')
-cln_fee = extract_field(1, 'CLN')
-ldk_fee = extract_field(1, 'LDK')
-ec1_fee = extract_field(1, 'Eclair_case1')
-ec2_fee = extract_field(1, 'Eclair_case2')
-ec3_fee = extract_field(1, 'Eclair_case3')
-
-lnd_dly = extract_field(2, 'LND')
-cln_dly = extract_field(2, 'CLN')
-ldk_dly = extract_field(2, 'LDK')
-ec1_dly = extract_field(2, 'Eclair_case1')
-ec2_dly = extract_field(2, 'Eclair_case2')
-ec3_dly = extract_field(2, 'Eclair_case3')
-
-lnd_lnt = extract_field(3, 'LND')
-cln_lnt = extract_field(3, 'CLN')
-ldk_lnt = extract_field(3, 'LDK')
-ec1_lnt = extract_field(3, 'Eclair_case1')
-ec2_lnt = extract_field(3, 'Eclair_case2')
-ec3_lnt = extract_field(3, 'Eclair_case3')
-
-lnd_srate = extract_field(4, 'LND')
-cln_srate = extract_field(4, 'CLN')
-ldk_srate = extract_field(4, 'LDK')
-ec1_srate = extract_field(4, 'Eclair_case1')
-ec2_srate = extract_field(4, 'Eclair_case2')
-ec3_srate = extract_field(4, 'Eclair_case3')
-
-dly_df = pd.DataFrame({'LND_dly':lnd_dly,'CLN_dly':cln_dly, 'LDK_dly':ldk_dly, 'Eclair_1_dly':ec1_dly, 'Eclair_2_dly':ec2_dly, 'Eclair_3_dly':ec3_dly})
-fee_df = pd.DataFrame({'LND':lnd_fee,'CLN':cln_fee, 'LDK':ldk_fee, 'Eclair_1':ec1_fee, 'Eclair_2':ec2_fee, 'Eclair_3':ec3_fee})
-lnt_df = pd.DataFrame({'LND':lnd_lnt,'CLN':cln_lnt, 'LDK':ldk_lnt, 'Eclair_1':ec1_lnt, 'Eclair_2':ec2_lnt, 'Eclair_3':ec3_lnt})
-srate_df = pd.DataFrame({'LND':lnd_srate,'CLN':cln_srate, 'LDK':ldk_srate, 'Eclair_1':ec1_srate, 'Eclair_2':ec2_srate, 'Eclair_3':ec3_srate})
-
-amounts = df['Amount']
+df1 = pd.DataFrame()
+df1['amount'] = df['Amount']
+algo = {'LND', 'CLN','LDK', 'Eclair_case1', 'Eclair_case2', 'Eclair_case3'}
+for a in algo:
+    df1[f'{a}fee'] = extract_field(1, a)
+    df1[f'{a}dly'] = extract_field(2, a)
+    df1[f'{a}pthlnt'] = extract_field(3, a)
+    df1[f'{a}tp'] = extract_field(4, a)
 
 
-data = pd.concat([dly_df, fee_df], axis=1)
+srate = {}
+frate = {}
+start = 0
+end = 8
+step = 1
+for a in algo:
+    slist = []
+    flist = []
+    amt_bins = []
+    for i in range(start, end, step):
+        if i == 0:
+            lrange = 0
+        else:
+            lrange = 10**i
+        rrange = 10**(i+1)
+        data = df1[(df1['amount']>lrange) & (df1['amount']<=rrange)]
+        amt_bins.append(len(data))
+        slist.append(len(data[data[f'{a}tp'] == 'Success']))
+        flist.append(len(data[data[f'{a}tp'] == 'Failure']))
+    
+    srate[a] = slist
+    frate[a] = flist
+    
+df1 = pd.DataFrame(frate)
+df1['Amount'] = amt_bins
+    
+# df1.to_csv('filtered_data_fail.csv')
 
-sns.displot(data=data,kind='ecdf', log_scale=True)
-plt.xlabel('Fee')
-plt.ylabel('ECDF')
+df1.plot(kind='line')
+box_pts = 10
+box = np.ones(box_pts)/box_pts
+x = np.arange(end)
+plt.bar(x, np.convolve(df1['LND'], box, mode='same'))
 plt.show()
-
-sns.displot(data=dly_df,kind='ecdf', log_scale=True)
-plt.xlabel('Delay')
-plt.ylabel('ECDF')
-plt.show()
-
-sns.displot(data=lnt_df,kind='ecdf', log_scale=True)
-plt.xlabel('Path Length')
-plt.ylabel('ECDF')
-plt.show()
-
-# sns.catplot(data=lnd_srate, x=)
-# plt.xlabel('Success Rate')
-plt.show()
-
-
-
 
 
