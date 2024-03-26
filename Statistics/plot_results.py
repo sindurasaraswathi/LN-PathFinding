83e449cb72021fd5cc0daa43e6debd66422d9374
sindurasaraswathi <sindurasaraswathi@gmail.com>
2024-03-26 11:29:22 -0400

    updated fee graph

--- Statistics/plot_results.py
+++ Statistics/plot_results.py
@@ -16,8 +16,9 @@ from tabulate import tabulate
 from ordered_set import OrderedSet
 
 plt.style.use('ggplot')
-df = pd.read_csv('/Users/ssarasw2/Desktop/LN pathfinding/LN-PathFinding/New_MP_results/LN_results_10k.csv')
-# df = pd.read_csv('C:/Users/sindu/Work/UNCC Research/GIT_LN/LN-PathFinding/New_MP_results/LN_results_10k.csv')
+file = 'LN_results_p_w'
+# df = pd.read_csv('/Users/ssarasw2/Desktop/LN pathfinding/LN-PathFinding/New_MP_results/LN_results_10k.csv')
+df = pd.read_csv(f'C:/Users/sindu/Work/UNCC Research/GIT_LN/LN-PathFinding/New_MP_results/{file}.csv')
 df = df.fillna("[[],0,0,0,'Failure']")
 df = df.drop_duplicates()
 
@@ -54,14 +55,12 @@ color = ['#1f77b4',  # Blue
                '#17becf']  # Cyan
 #-------------------------------------------------------------------------------------------------
 srate = {}
-frate = {}
 start = 0
-end = 8
+end = 8 #6 for fair
 step = 1
 #calculate the count of success and failure in the bin of amount
 for a in algo:
     slist = []
-    flist = []
     amt_bins = []
     for i in range(start, end, step):
         if i == 0:
@@ -72,39 +71,36 @@ for a in algo:
         data = df1[(df1['amount']>lrange) & (df1['amount']<=rrange)]
         amt_bins.append(len(data))
         slist.append(len(data[data[f'{a}tp'] == 'Success']))
-        flist.append(len(data[data[f'{a}tp'] == 'Failure']))
     srate[a] = slist
-    frate[a] = flist 
-srate['Amount'] = amt_bins
-frate['Amount'] = amt_bins
+srate['Txn Count'] = amt_bins
 
 #-------------------------------------------------------------------------------------------------
 #scale df to 100
 sdf = pd.DataFrame(srate)
-fdf = pd.DataFrame(frate)
 for a in algo:
-    sdf[a] = sdf[a]*100/sdf['Amount']
-sdf['Amount'] = sdf['Amount']*100/sdf['Amount']
+    sdf[a] = sdf[a]*100/sdf['Txn Count']
+sdf['Txn Count'] = sdf['Txn Count']*100/sdf['Txn Count']
 # sdf['Bins'] = [f'{i}-{i+1}' for i in range(8)]
-sdf[sdf.columns[0:-1]].plot(kind='bar', color=color)
+sdf[sdf.columns[0:-1]].plot(kind='bar', color=color) #bar plot
 plt.xlabel('Amount bins')
 plt.ylabel('percentage')
 plt.title('Success Rate percentage')
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
 
-sdf['Actual Amount'] = amt_bins
+sdf['Actual Txn Count'] = amt_bins
 # print(sdf.to_latex(index=False))
 print('\n', tabulate(sdf, headers = 'keys', tablefmt = 'psql',showindex=True))
 print('\nSuccess Rate:\n\n', tabulate(srate, headers = 'keys', tablefmt = 'psql',showindex=False))
 # print('\nFailure Rate:\n\n', tabulate(frate, headers = 'keys', tablefmt = 'psql',showindex=False))
 
 #-------------------------------------------------------------------------------------------------
+# plot data frame (line graph), pass dict to the function
 def df_plot(data, amt_bins, algo, title, xlabel, ylabel):
     df_temp = pd.DataFrame(data)
     ratio_df = pd.DataFrame()
     for a in algo:
-        ratio_df[a] = df_temp[a]/df_temp['Amount']
+        ratio_df[a] = df_temp[a]/df_temp['Txn Count']
     ratio_df.plot(color=color)
     # df_temp.plot(kind='bar')
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
@@ -113,9 +109,10 @@ def df_plot(data, amt_bins, algo, title, xlabel, ylabel):
     plt.ylabel(ylabel)
     plt.show()
 
-df_plot(srate, amt_bins, algo, 'Success Rate', 'Amount bins', 'Ratio')
-# df_plot(frate, amt_bins, algo, 'Failure Rate', 'Amount bins', 'Ratio')
-pd.DataFrame(srate).plot(kind='bar', color=color)
+df_plot(srate, amt_bins, algo, 'Success Rate', 'Amount bins', 'Ratio') 
+
+
+pd.DataFrame(srate).plot(kind='bar', color=color) #bar
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.title('Success Rate')
 plt.xlabel('Amount bins')
@@ -139,9 +136,8 @@ def plot_graph(x, y, kind, xlog, ylog, title, xlabel, ylabel):
     plt.ylabel(ylabel)
     plt.show()
     
-    
+#find median fee in the amount bin, return list of list of fees in bins, list of median fee   
 def fee_df(val, name, step):
-    end = 8
     fee_list = []
     count = []
     fee_med = []
@@ -163,34 +159,38 @@ def fee_df(val, name, step):
         fee_list.append(list(data[name]))
         amount_list.append(list(data['amount']))
     return fee_list, fee_med, amount_list
-    
+ 
+#filter data based on success rate across variants
+#If success on 5 or more algo, apppend to sfee
+sfee = pd.DataFrame(columns=df1.columns)
+for i in df1.index:
+    c = 0
+    row = df1.loc[i]
+    for a in algo:
+        if row[f'{a}tp'] == 'Success':
+            c+=1
+    if c>4:
+        sfee = sfee.append(row, ignore_index=True)   
      
+save_df = pd.DataFrame(index=['Weighted avg median fee', 'WAvg path length', 'WAvg Delay', 'avg_path', 'Avg delay'])
 for a in algo:
-    pdf = pd.DataFrame(columns=['avg amount', 'avg path length', 'median fee', 'avg delay'])
-    grp_val = df1[[f'{a}fee', 'amount']].sort_values('amount').groupby('amount')
-    pth_grp = df1[df1[f'{a}tp']=='Success'][[f'{a}pthlnt', 'amount']].sort_values('amount').groupby('amount').mean()
-    dly_grp = df1[df1[f'{a}tp']=='Success'][[f'{a}dly', 'amount']].sort_values('amount').groupby('amount').mean()
-    for fltr in ['Success']:
-        if fltr == 'Success':
-            val = df1[df1[f'{a}tp']=='Success'][[f'{a}fee', 'amount']]
-        else:
-            val = df1[[f'{a}fee', 'amount']]
-        name = f'{a}fee'
-        step = 1
-        fee_list, fee_med, amount_list = fee_df(val, name, step)
-        if fltr == 'Success':
-            fval = fee_med
-        plot_graph(fee_list, 0, 'box', False, True, f'{a} Fee', 'Amount', 'Fee')
-        plot_graph(range(len(fee_med)), fee_med,'scatter', False, True, f'{a} Median Fee', 'Amount', 'Fee')
+    pdf = pd.DataFrame(columns=['median amount', 'avg path length', 'median fee', 'avg delay'])
+    grp_val = sfee[[f'{a}fee', 'amount']].sort_values('amount').groupby('amount')
+    pth_grp = sfee[[f'{a}pthlnt', 'amount']].sort_values('amount').groupby('amount').mean()
+    dly_grp = sfee[[f'{a}dly', 'amount']].sort_values('amount').groupby('amount').mean()
+
+    val = sfee[[f'{a}fee', 'amount']]
+    name = f'{a}fee'
+    step = 1
+    fee_list, fee_med, amount_list = fee_df(val, name, step)
+    plot_graph(fee_list, 0, 'box', False, True, f'{a} Fee', 'Amount', 'Fee')
+    plot_graph(range(len(fee_med)), fee_med,'scatter', False, True, f'{a} Median Fee', 'Amount', 'Fee')
         
-    val = []
-    key = []
-    for i, j in grp_val:
-        if i%50 == 0 and i<=1000 and i>=100:
-            val.append(list(j[f'{a}fee']))
-            key.append(i)
-                                  
-    for j in range(8):
+    w_sum = 0 
+    p_sum = 0
+    d_sum = 0
+    total_weight = 0                          
+    for j in range(end):
         pval = []
         pkey = []
         pdly = []
@@ -200,27 +200,27 @@ for a in algo:
                 pval.append(pth_grp.loc[i][f'{a}pthlnt'])
                 pdly.append(dly_grp.loc[i][f'{a}dly'])
         if pkey == []:
-            pdf.loc[j] = ['','','', '']
+            continue
         else:
-            pdf.loc[j] = [mean(pkey), mean(pval), fval[j], mean(pdly)]
-    pdf.loc[j+1] = [mean(pdf['avg amount']), mean(pdf['avg path length']), mean(pdf['median fee']), mean(pdf['avg delay'])]
+            pdf.loc[j] = [median(pkey), mean(pval), fee_med[j], mean(pdly)]
+            weight = len(fee_list[j])
+            total_weight += weight
+            w_sum = w_sum + weight*fee_med[j]
+            p_sum = p_sum + weight*mean(pval)
+            d_sum = d_sum + weight*mean(pdly)
+    weighted_median = w_sum/total_weight
+    weighted_plength = p_sum/total_weight
+    weighted_dly = d_sum/total_weight
     print(a,'\n', tabulate(pdf, headers = 'keys', tablefmt = 'psql',showindex=True))
-
+    print("Weighted average of median fee:", weighted_median) #(weight is the number of transactions in the bin)
+    print('Weighted average path length:', weighted_plength)
+    print('Weighted average Timelock:', weighted_dly)
+    print("Average Path Length:", mean(pdf['avg path length']))
+    print("Average Timelock:", mean(pdf['avg delay']))
+    save_df[a] = [weighted_median, weighted_plength, weighted_dly, mean(pdf['avg path length']), mean(pdf['avg delay'])]
     
-        # for i in range(len(amount_list)):
-        #     amt_list = list(OrderedSet(amount_list[i]))
-        #     key.append(amt_list)
-        #     temp = []
-        #     for j in amt_list:
-        #         temp.append(list(grp_val.get_group(j)[f'{a}fee']))
-        #     val.append(temp)
-# plot_graph(val, 0, 'box', False, False, f'{a} Fee vs Amount ({fltr})', 'Amount', 'Fee')
-
-    # plt.boxplot(val, showfliers=False)
-    # plt.title(a)
-    # plt.xticks(range(1,len(key)+1), key)
-    # plt.show()
-
+save_df.to_csv(f'{file}_stat2.csv')
+sdf.to_csv(f'{file}_stat1.csv')
 #-------------------------------------------------------------------------------------------------
 def sns_plot(data, kind, xlog, ylog, title, xlabel, ylabel):
     fig, ax = plt.subplots(figsize=plt.figaspect(1/3))
@@ -239,35 +239,22 @@ def sns_plot(data, kind, xlog, ylog, title, xlabel, ylabel):
     plt.show()
 
 
-data = df1[[f'{a}pthlnt' for a in algo]]
-# for a in algo:
-#     data = df1[df1[f'{a}tp']=='Success'][f'{a}pthlnt']
-#     path_avg.append(data.mean())
-# print(path_avg)
+data = sfee[[f'{a}pthlnt' for a in algo]]
 sns_plot(data, 'hist', False, False, 'Path Length', 'Path Length', 'Count')
 # sns_plot(data, 'kde', False, False, 'Path length (KDE)', '', '')
+
+
+# data = sfee[[f'{a}dly' for a in algo]]
+# print(data.mean())
+# data=sfee[[f'{a}fee' for a in algo]]
 # print(data.mean())
 
 
-data = df1[[f'{a}dly' for a in algo]]
-print(data.mean())
-data=df1[[f'{a}fee' for a in algo]]
-print(data.mean())
-
-
 #-------------------------------------------------------------------------------------------------
-# def save_df(data, filename):
-#     df_temp = pd.DataFrame(data) #dict data
-#     df_temp.to_csv(filename)
+
+#------------------------------------------------------------------------------------------------
+
+        
+
     
-# save_df(srate, 'success_count.csv')
-# save_df(frate, 'Failure_count.csv')
-# df1.to_csv('result_data.csv')
-#--------------------------------------------------------------------------------------------
-# max_suc = []
-# for a in algo:
-#     print(a)
-#     succ = sum(srate[a])/100
-#     max_suc.append(succ)
-#     print(succ)
-# print(max(max_suc))
+    
